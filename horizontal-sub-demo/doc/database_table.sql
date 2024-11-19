958da1cecf4cb3570c7a87cdf659853aea149f76
dyrnq <dyrnq@qq.com>
2024-11-19 17:32:31 +0800

    Add course_0 table

--- horizontal-sub-demo/doc/database_table.sql
+++ horizontal-sub-demo/doc/database_table.sql
@@ -5,6 +5,12 @@ create database IF NOT EXISTS course_db_1;
 create database IF NOT EXISTS course_db_2;
 -- 在两个库中分别创建四个结构相同的表
 use course_db_1;
+create table course_0
+(
+    id     bigint(20) primary key,
+    name   varchar(50),
+    status TINYINT
+);
 
 create table course_1
 (
@@ -35,6 +41,13 @@ create table course_4
 );
 
 use course_db_2;
+
+create table course_0
+(
+    id     bigint(20) primary key,
+    name   varchar(50),
+    status TINYINT
+);
 
 create table course_1
 (
