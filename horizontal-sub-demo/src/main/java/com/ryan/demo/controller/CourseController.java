ea58896c7d62e4964b6dadd0d8a99b44ccd75634
dyrnq <dyrnq@qq.com>
2024-11-20 09:51:57 +0800

    Compatible add createdAt post add

--- horizontal-sub-demo/src/main/java/com/ryan/demo/controller/CourseController.java
+++ horizontal-sub-demo/src/main/java/com/ryan/demo/controller/CourseController.java
@@ -1,10 +1,12 @@
 package com.ryan.demo.controller;
 
+import cn.hutool.core.date.DateTime;
+import cn.hutool.core.date.DateUtil;
 import com.baomidou.mybatisplus.core.metadata.IPage;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.ryan.demo.domain.Course;
 import com.ryan.demo.service.CourseService;
 import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,18 +20,33 @@ import java.util.List;
  **/
 @RestController
 @RequestMapping("/course")
+@Slf4j
 public class CourseController {
 
     @Resource
     private CourseService service;
 
     @PostMapping("/add")
-    public void add(Long id){
+    public void add(Long id, String createdAt) {
         Course course = new Course();
         course.setId(id);
         course.setName("java");
         course.setStatus(0);
+        if (createdAt != null) {
+            log.info("createdAt:{}", createdAt);
+            DateTime dt = DateUtil.parse(createdAt,
+                    "yyyy-MM-dd HH:mm:ss",
+                    "yyyy-MM-dd HH:mm:ss.SSS",
+                    "yyyy-MM-dd HH:mm:ss.SSSSSS"
+            );
+            course.setCreatedAt(dt);
+        }
         service.save(course);
+    }
+
+    @GetMapping("/del")
+    public void del(Long id) {
+        service.removeById(id);
     }
 
     @GetMapping("/all")
